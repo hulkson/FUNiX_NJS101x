e6a645ebccd4029a3cac4a83751bd7813253cb20
hulkson <quy.qtp@gmail.com>
2022-10-25 01:21:17 +0900

    Lab 4.4 ~ 4.6

---
+++ server.js
@@ -0,0 +1,23 @@
+// Lưu trữ dữ liệu trong tệp thông qua model
+
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const path = require("path");
+
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
+const errorController = require('./controllers/error')
+
+app.set("view engine", "ejs");
+app.set("views", "views");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use("/admin", adminRoutes);
+app.use(shopRoutes);
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use(errorController.get404);
+
+app.listen(3000);
