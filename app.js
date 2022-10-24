d9977e3ccc017eef2337e8bc35aacdf1cfef5218
hulkson <quy.qtp@gmail.com>
2022-10-25 01:30:10 +0900

    Lab 5.18 ~ 5.28

---
+++ app.js
@@ -0,0 +1,77 @@
+const path = require("path");
+
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const errorController = require("./controllers/error");
+const sequelize = require("./util/database");
+
+// import model đề tạo quan hệ bảng trong database phía dưới
+const Product = require("./models/product");
+const User = require("./models/users");
+const Cart = require("./models/cart");
+const cartItem = require("./models/cart-Item");
+const Order = require("./models/order");
+const oderItem = require("./models/order-item");
+
+const app = express();
+
+app.set("view engine", "ejs");
+app.set("views", "views");
+
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
+
+// add middleware
+app.use((req, res, next) => {
+  User.findByPk(1)
+    .then((user) => {
+      req.user = user;
+      next();
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+app.use("/admin", adminRoutes);
+app.use(shopRoutes);
+
+app.use(errorController.get404);
+
+// add quan hệ cho các bảng
+Product.belongsTo(User, { constraints: true, onDelete: "CASCADE" });
+User.hasMany(Product);
+User.hasOne(Cart);
+Cart.belongsTo(User);
+Cart.belongsToMany(Product, { through: cartItem });
+Product.belongsToMany(Cart, { through: cartItem });
+Order.belongsTo(User);
+User.hasMany(Order);
+Order.belongsToMany(Product, {through: oderItem});
+
+
+sequelize
+  // .sync({force: true})
+  .sync()
+  .then((result) => {
+    return User.findByPk(1);
+  })
+  .then((user) => {
+    if (!user) {
+      return User.create({ name: "Max", email: "test@test.com" });
+    }
+    return user;
+  })
+  .then((user) => {
+    return user.createCart();
+  })
+  .then((cart) => {
+    app.listen(3000);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
