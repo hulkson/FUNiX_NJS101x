9b5053e07b90de33f56d7598aabdf89bf0a1a63e
hulkson <quy.qtp@gmail.com>
2022-10-25 01:13:24 +0900

    Lab 3.1 ~ 3.5

---
+++ server.js
@@ -0,0 +1,24 @@
+// Thêm layout
+
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const path = require("path");
+
+const adminData = require("./routes/admin");
+const shopRouter = require("./routes/shop");
+
+app.set("view engine", "pug");
+app.set("views", "views");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use("/admin", adminData.routes);
+app.use(shopRouter);
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use((req, res, next) => {
+  res.status(404).render("404", { pageTitle: "404 not found!" });
+});
+
+app.listen(3000);
