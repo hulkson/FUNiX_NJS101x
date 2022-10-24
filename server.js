b4450cd80751faee3904e72e717aeb2fd90d8251
hulkson <quy.qtp@gmail.com>
2022-10-25 01:11:09 +0900

    lab 2.10 ~ 2.14

---
+++ server.js
@@ -0,0 +1,21 @@
+// Cung cấp tệp tĩnh
+
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const path = require("path");
+
+const adminRouter = require("./routes/admin");
+const shopRouter = require("./routes/shop");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use("/admin", adminRouter);
+app.use(shopRouter);
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use((req, res, next) => {
+  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
+});
+
+app.listen(3000);
