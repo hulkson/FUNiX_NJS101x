01222ca0ad009875005e4aa96e1191e833942b74
hulkson <quy.qtp@gmail.com>
2022-12-11 23:51:03 +0900

    completed signup page

--- controllers/auth.js
+++ controllers/auth.js
@@ -7,6 +7,14 @@ exports.getLogin = (req, res) => {
       isAuth: false,
    })
  };
+
+ exports.getSignup = (req, res) => {
+  res.render('../views/auth/signup', {
+     path: '/signup',
+     pageTitle: 'signup',
+     isAuth: false,
+  })
+};
  
  exports.postLogin = (req, res) => {
   User.findById('62dec61e987b0e7dcad0c52f')
@@ -18,6 +26,30 @@ exports.getLogin = (req, res) => {
     .catch(error => {
       console.log(error);
     })
+ };
+
+ exports.postSignup = (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+  const confirmPassword = req.body.confirmPassword;
+  User.findOne({ email: email })
+    .then(userDoc => {
+      if (userDoc) {
+        return res.redirect('/signup');
+      }
+      const user = new User({
+        email: email,
+        password: password,
+        cart: { items: [] }
+      });
+      return user.save();
+    })
+    .then(result => {
+      res.redirect('/login');
+    })
+    .catch(err => {
+      console.log(err);
+    });
  };
 
  exports.postLogout = (req, res) => {
