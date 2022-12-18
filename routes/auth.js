5619672b591bff9582175d3b09da173178295248
hulkson <quy.qtp@gmail.com>
2022-12-18 19:45:42 +0900

    push Lab 9.13

---
+++ routes/auth.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const { check, body } = require('express-validator');
+
+const authController = require('../controllers/auth');
+const User = require('../models/user');
+
+const router = express.Router();
+
+router.get('/login', authController.getLogin);
+
+router.get('/signup', authController.getSignup);
+
+router.post(
+   '/login',
+   [
+     body('email')
+       .isEmail()
+       .withMessage('Please enter a valid email address.')
+       .normalizeEmail(),
+     body('password', 'Password has to be valid.')
+       .isLength({ min: 5 })
+       .isAlphanumeric()
+       .trim(),
+   ],
+   authController.postLogin
+ );
+
+router.post('/signup', 
+   [
+      check('email')
+      .isEmail()
+      .withMessage('Please enter a valid email.')
+      .custom((value, { req }) => {
+        return User.findOne({ email: value }).then(userDoc => {
+          if (userDoc) {
+            return Promise.reject(
+              'E-Mail exists already, please pick a different one.'
+            );
+          }
+        });
+      })
+      .normalizeEmail(),
+      body('password')
+         .isAlphanumeric()
+         .isLength({ min: 5 })
+         .withMessage('enter password with number and text at least 5 characters')
+         .trim(),
+      body('confirmPassword')
+         .trim()
+         .custom((value, { req }) => {
+            if (value !== req.body.password) {
+               throw new Error('password do not match')
+            }
+            return true;
+         })
+   ], authController.postSignup);
+
+router.post('/logout', authController.postLogout);
+
+module.exports = router;
